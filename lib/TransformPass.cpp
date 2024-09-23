b2342f78049f1b691518418c0f33ca0af3439e42
nouman-10x <noumanamir453@gmail.com>
2024-09-23 10:43:57 +0500

    Cleaned directory

--- lib/TransformPass.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pass.h"
-
-using namespace llvm;
-
-namespace pass {
-    PreservedAnalyses TransformPass::run(Function &F, FunctionAnalysisManager &FAM) {
-        return PreservedAnalyses::all();
-    }
-}
